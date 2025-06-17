91bb14bb8e5dc570a77ec8332d8d583b7b54f571
Pradip Ghadge <121886588+GhadgePradip9971@users.noreply.github.com>
2025-06-17 19:29:11 +0530

    MVC_Boot_Project(requestPath)

---
+++ BootMVC_proj_3-RequestPath/src/main/java/com/web/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.web.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/")
+	public String showHome() {
+		return "home";
+	}
+	
+	
+	@RequestMapping(value="/report" ,method=RequestMethod.GET)
+	
+	public String showReport(Map<String,Object>map) {
+		System.out.println("TestController.showreport");
+		return "display";
+		
+	}
+
+	
+	@RequestMapping(value="/report",method=RequestMethod.POST)
+	public String showReport1(Map<String,Object>map) {
+		System.out.println("TestController.showreport1");
+		return "display1";
+		
+	}
+	
+	
+	
+}
